b14570e01db7102e27abff79f585c8baccd0ccd3
shakhzodbekbobobekov <shaxzodbekbobobekov@gmail.com>
2022-08-03 22:48:54 +0500

    add search components

---
+++ src/search/Search.js
@@ -0,0 +1,21 @@
+import { useLocation } from "react-router-dom";
+import { useFetch } from "../components/hooks/useFetch";
+import RicipeList from "../components/RicipeList";
+
+function Search() {
+  const useParams = useLocation().search;
+  const useString = new URLSearchParams(useParams);
+  const getParams = useString.get("q");
+  const url = "http://localhost:3000/products?q=" + getParams;
+  const { data } = useFetch(url);
+  return (
+    <div>
+      <div className="page-title">
+        Bu siz izlagan mahsulot turi: <i>{getParams}</i>
+      </div>
+      {data && <RicipeList data={data} />}
+    </div>
+  );
+}
+
+export default Search;
