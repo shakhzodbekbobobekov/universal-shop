701448a3c989b0b2fc5e2d24a2af9513af10115f
shakhzodbekbobobekov <shaxzodbekbobobekov@gmail.com>
2022-08-07 23:59:07 +0500

    add navbar components correcting mistakes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,15 +2,16 @@ import "./Navbar.css";
 import { Link } from "react-router-dom";
 import Searchbar from "./Searchbar";
 import Select from "../select/Select";
-
-function Navbar() {
+import { useTheme } from "../components/hooks/useTheme";
+function Navbar({ setCategoryAll }) {
+  const { color } = useTheme();
   return (
-    <div className="navbar">
+    <div className="navbar" style={{ background: color }}>
       <nav>
         <Link to="/" className="brand">
           <h1>Shopping</h1>
         </Link>
-        <Select />
+        <Select setCategoryAll={setCategoryAll} />
         <Searchbar />
         <Link to="/create">Create Now</Link>
       </nav>
